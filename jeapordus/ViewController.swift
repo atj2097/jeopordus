f3174cf209cf03a76e21545c379870a27a2d3cd9
Adam <adamjackson@pursuit.org>
2019-09-13 15:20:04 -0400

    Initialization

---
+++ jeapordus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jeapordus
+//
+//  Created by God on 9/13/19.
+//  Copyright © 2019 Jeapordus Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
