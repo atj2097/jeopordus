eec4260968dc5d990d19a673508b39d44925673c
OliveiraAlbertCoelho <albertoliveira@pursuit.org>
2019-09-17 14:47:10 -0400

    made random array and gave functionality to buttons

--- jeapordus/DVC/JeopDVC.swift
+++ jeapordus/DVC/JeopDVC.swift
@@ -9,42 +9,43 @@
 import UIKit
 
 class JeopDVC: UIViewController {
-
-    @IBOutlet weak var Question: UITextView!
     
-
+    @IBOutlet weak var Question: UITextView!
     @IBOutlet weak var answers: UIButton!
     var buttonid: Int?
     var modeChoice: String?
+    var shuffledAnswers = [String]()
     var triviaInfo: Trivia?{
         didSet{
-              Question.text = triviaInfo?.question
+            Question.text = triviaInfo?.question.removingPercentEncoding
+            shuffledAnswers = (self.triviaInfo?.answersShuffle())!
+            setUpButtons()
         }
     }
     
-
-
-
     
     @IBOutlet var buttonAnswers: [UIButton]!
-    
-    @IBAction func buttonAnswerAct(_ sender: Any) {
+    @IBAction func buttonAnswerAct(_ sender: UIButton) {
+        print(sender.titleLabel?.text)
+        print(buttonAnswers.count)
     }
-    
-
     override func viewWillAppear(_ animated: Bool) {
         loadData(mode: modeChoice!, id: buttonid!)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
         Question.text = triviaInfo?.question
-
-
-        // Do any additional setup after loading the view.
-
     }
+    func setUpButtons(){
+        for (k,v) in shuffledAnswers.enumerated(){
+            print("\(k)im k")
+            buttonAnswers[k].setTitle(v, for: .normal)
+            print("\(v)im v")
+         
+        }
+    }
+    
+    
     private func loadData(mode: String, id: Int){
         TriviaWrapper.fetchTriviaData(mode: mode, id: id){ (result) in
             DispatchQueue.main.async {
@@ -58,5 +59,5 @@ class JeopDVC: UIViewController {
             }
         }
     }
-
+    
 }
