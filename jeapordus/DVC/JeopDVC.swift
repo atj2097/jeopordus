2fbf2ca2320a5da787fc4e354ff9c50968818cb5
Adam <adamjackson@pursuit.org>
2019-09-17 14:45:06 -0400

    Fixed button array, added auto layout

--- jeapordus/DVC/JeopDVC.swift
+++ jeapordus/DVC/JeopDVC.swift
@@ -11,9 +11,9 @@ import UIKit
 class JeopDVC: UIViewController {
 
     @IBOutlet weak var Question: UITextView!
+    @IBOutlet var buttonAnswers: [UIButton]!
     
 
-    @IBOutlet weak var answers: UIButton!
     var buttonid: Int?
     var modeChoice: String?
     var triviaInfo: Trivia?{
@@ -26,21 +26,36 @@ class JeopDVC: UIViewController {
 
 
     
-    @IBOutlet var buttonAnswers: [UIButton]!
+
     
     @IBAction func buttonAnswerAct(_ sender: Any) {
     }
     
+    @IBOutlet var countDownLabel: UILabel!
+    
+    var count = 10
 
+        
+    
+    
+//    func update() {
+//        if(count > 0) {
+//            countDownLabel.text = String(count-=1)
+//        }
+//    }
     override func viewWillAppear(_ animated: Bool) {
         loadData(mode: modeChoice!, id: buttonid!)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+//        var timer = Timer.scheduledTimer(timeInterval: 0.4, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
+//        
         
         Question.text = triviaInfo?.question
-
+        Question.font = UIFont(name: "MarkerFelt-Wide", size: 30)
+        for buttons in buttonAnswers {
+            buttons.titleLabel?.font = UIFont(name: "MarkerFelt-Wide", size: 30)
+        }
 
         // Do any additional setup after loading the view.
 
