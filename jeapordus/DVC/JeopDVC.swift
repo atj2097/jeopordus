d9b0e2d09be7ae50ea5495f258a23a9cbca0e14d
OliveiraAlbertCoelho <albertoliveira@pursuit.org>
2019-09-17 15:21:28 -0400

    added functionality to dvc controller

--- jeapordus/DVC/JeopDVC.swift
+++ jeapordus/DVC/JeopDVC.swift
@@ -9,17 +9,10 @@
 import UIKit
 
 class JeopDVC: UIViewController {
-<<<<<<< HEAD
+    
     
     @IBOutlet weak var Question: UITextView!
-    @IBOutlet weak var answers: UIButton!
-=======
-
-    @IBOutlet weak var Question: UITextView!
-    @IBOutlet var buttonAnswers: [UIButton]!
     
-
->>>>>>> 04bb83f9ed32840957b382ed49dc1e9cd3619d2f
     var buttonid: Int?
     var modeChoice: String?
     var shuffledAnswers = [String]()
@@ -32,58 +25,55 @@ class JeopDVC: UIViewController {
     }
     
     
-<<<<<<< HEAD
     @IBOutlet var buttonAnswers: [UIButton]!
     @IBAction func buttonAnswerAct(_ sender: UIButton) {
-        print(sender.titleLabel?.text)
-        print(buttonAnswers.count)
+        if sender.titleLabel?.text == triviaInfo?.correctAnswer{
+            
+            
+        }else {
+            view.backgroundColor = .red
+            
+        }
     }
-=======
-
     
-    @IBAction func buttonAnswerAct(_ sender: Any) {
-    }
+    
     
     @IBOutlet var countDownLabel: UILabel!
     
     var count = 10
-
-        
     
     
-//    func update() {
-//        if(count > 0) {
-//            countDownLabel.text = String(count-=1)
-//        }
-//    }
->>>>>>> 04bb83f9ed32840957b382ed49dc1e9cd3619d2f
+    
+    
+    //    func update() {
+    //        if(count > 0) {
+    //            countDownLabel.text = String(count-=1)
+    //        }
+    //    }
+    
     override func viewWillAppear(_ animated: Bool) {
         loadData(mode: modeChoice!, id: buttonid!)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-<<<<<<< HEAD
+        
         Question.text = triviaInfo?.question
-=======
-//        var timer = Timer.scheduledTimer(timeInterval: 0.4, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
-//        
+        //        var timer = Timer.scheduledTimer(timeInterval: 0.4, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
+        //
         
         Question.text = triviaInfo?.question
         Question.font = UIFont(name: "MarkerFelt-Wide", size: 30)
         for buttons in buttonAnswers {
             buttons.titleLabel?.font = UIFont(name: "MarkerFelt-Wide", size: 30)
         }
-
+        
         // Do any additional setup after loading the view.
-
->>>>>>> 04bb83f9ed32840957b382ed49dc1e9cd3619d2f
+        
     }
     func setUpButtons(){
         for (k,v) in shuffledAnswers.enumerated(){
-            print("\(k)im k")
             buttonAnswers[k].setTitle(v, for: .normal)
-            print("\(v)im v")
-         
+            
         }
     }
     
